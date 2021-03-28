f0b86218ea4c6417663166fad73e66f0e6e9baba
DESKTOP-KC8155E\Lenovo <EasterEggs1230@gmail.com>
2021-03-28 17:46:55 +0700

    คำตอบข้อสอบปฏิบัติข้อที่ 2 คูณร่วมน้อย (1.5 คะแนน)...

---
+++ Program (Test Answer No.2).cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Test_Answer_No._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int x = a; int y = b;
+
+            while (x != y)
+            {
+                while (x < y)
+                {
+                    x = x + a;
+                }
+                while (x > y)
+                {
+                    y = y + b;
+                }
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
